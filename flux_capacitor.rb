0df03409c5e74504c62d2736461c2255217c4551
alexanderjsingleton <alexanderjsingleton@gmail.com>
2016-02-25 12:54:16 -0600

    flux capacitor

--- flux_capacitor.rb
+++ flux_capacitor.rb
@@ -1,8 +1,19 @@
-require 'time'
+# https://www.ruby-forum.com/topic/125709
+# https://www.ruby-forum.com/topic/104359
+# http://apidock.com/ruby/String/insert
+
+# require 'time'
+
+# def flux_capacitor(time,mins)
+#   the_time = Time.parse(time.to_s)
+#   mins = the_time + 10*60
+# end
 
 def flux_capacitor(time,mins)
-  the_time = Time.parse(time.to_s)
-  mins = the_time + 10*60
+  the_time = time.scan(/\d/).join('').to_i
+  meridian = time.scan(/(A|M|P)/).join('')
+  new_time = the_time + mins 
+  back_in_time = new_time.to_s.insert(-3,':') + " #{meridian}"
 end
 
 puts flux_capacitor("9:13 AM",10)
